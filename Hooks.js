dc4844f014216e334af3f2d9707bd15d375532fd
Xu Yiming <xuyiming.open@outlook.com>
2016-04-12 22:25:21 +0800

    refactor(Hooks.js):

--- Hooks.js
+++ Hooks.js
@@ -1,105 +1,132 @@
-var Hooks = {
-	"fn": function fn( $fn, onInvoke ) {	// parameter fn refers to function fn( $fn, onInvoke ) itself
-		"use strict";
+// ==UserScript==
+// @name			Hooks
+// @namespace		xuyiming.open@outlook.com
+// @description		A JavaScript hook/proxy/reflect ultility
+// @author			xymopen
+// @version			1.1.0
+// @grant			none
+// @license			BSD 2-Clause
+// @homepageURL		https://github.com/xymopen/JS_Utilities/blob/master/Hooks.js
+// ==/UserScript==
 
-		return function() {
-			return onInvoke.call( this, $fn, arguments );
-		};
-	},
+// TODO: JsDoc
 
-	"property": function property( object, propertyName, onGet, onSet ) {
-		"use strict";
+var Hooks = ( function() {
+    "use strict";
 
-		var descriptor, oldValue;
+	var Hooks = {
+        "apply": function apply( target, onApply ) {
+			return function() {
+                return onApply.call( this, target, this, arguments );
+            };
+        },
 
-		if ( Object.prototype.hasOwnProperty.call( object, propertyName ) ) {
-			descriptor = Object.getOwnPropertyDescriptor( object, propertyName );
+        "property": function get( target, propertyName, onGet, onSet ) {
+            var descriptor, oldValue;
 
-			if ( Object.prototype.hasOwnProperty.call( descriptor, "value" ) ) {
-				oldValue = descriptor.value;
+            if ( Object.prototype.hasOwnProperty.call( target, propertyName ) ) {
+                descriptor = Object.getOwnPropertyDescriptor( target, propertyName );
 
-				delete descriptor.value;
-				delete descriptor.writable;
-			} else if ( Object.prototype.hasOwnProperty.call( descriptor, "get" ) ) {
-				oldValue = descriptor.get.call( object );
-			} else {
-				oldValue = undefined;
-			}
+                if ( Object.prototype.hasOwnProperty.call( descriptor, "value" ) ) {
+                    oldValue = descriptor.value;
 
-			descriptor.get = function get() {
-				return onGet.call( this, oldValue );
-			};
+                    delete descriptor.value;
+                    delete descriptor.writable;
+                } else if ( Object.prototype.hasOwnProperty.call( descriptor, "get" ) ) {
+                    oldValue = descriptor.get.call( target );
+                } else {
+                    oldValue = undefined;
+                }
 
-			descriptor.set = function set( newValue ) {
-				oldValue = onSet.call( this, oldValue, newValue );
-				return oldValue;
-			};
+                descriptor.get = function get() {
+                    return onGet.call( this, target, propertyName, oldValue );
+                };
 
-			Object.defineProperty( object, propertyName, descriptor );
-		} else {
-			throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
-		}
-	},
+                descriptor.set = function set( newValue ) {
+                    oldValue = onSet.call( this, target, propertyName, oldValue, newValue );
+                    return oldValue;
+                };
 
-	"getter": function getter( object, propertyName, onGet ) {
-		"use strict";
+                Object.defineProperty( target, propertyName, descriptor );
+            } else {
+                throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
+            }
+        },
 
-		var descriptor, $getter;	// variable getter refers to function getter( object, propertyName, onGet ) itself
+        "get": function get( target, propertyName, onGet ) {
+            return Hooks.property( target, propertyName, onGet, function( target, propertyName, oldValue, newValue ) {
+                return newValue;
+            } );
+        },
 
-		if ( Object.prototype.hasOwnProperty.call( object, propertyName ) ) {
-			descriptor = Object.getOwnPropertyDescriptor( object, propertyName );
-			$getter = descriptor.get;
+        "set": function set( target, propertyName, onSet ) {
+            return Hooks.property( target, propertyName, function( target, propertyName, oldValue ) {
+                return oldValue;
+            }, onSet );
+        },
 
-			if ( Object.prototype.hasOwnProperty.call( descriptor, "get" ) &&
-				typeof $getter === "function" ) {
-				descriptor.get = Hooks.fn( $getter, onGet );
-			} else {
-				throw new Error( "ERR_NOT_A_GETTER" );
-			}
+        "getter": function getter( target, propertyName, onGetter ) {
+            var descriptor, getter;
 
-			Object.defineProperty( object, propertyName, descriptor );
-		} else {
-			throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
-		}
-	},
+            if ( Object.prototype.hasOwnProperty.call( target, propertyName ) ) {
+                descriptor = Object.getOwnPropertyDescriptor( target, propertyName );
+                getter = descriptor.get;
 
-	"setter": function setter( object, propertyName, onSet ) {
-		"use strict";
+                if ( Object.prototype.hasOwnProperty.call( descriptor, "get" ) &&
+                    "function" === typeof getter ) {
+                    descriptor.get = Hooks.apply( getter, function( getter, thisArg, argv ) {
+                        return onGetter.call( this, target, propertyName, getter, thisArg, argv );
+                    } );
+                } else {
+                    throw new Error( "ERR_NOT_A_GETTER" );
+                }
 
-		var descriptor, $setter;	// variable setter refers to function setter( object, propertyName, onSet ) itself
+                Object.defineProperty( target, propertyName, descriptor );
+            } else {
+                throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
+            }
+        },
 
-		if ( Object.prototype.hasOwnProperty.call( object, propertyName ) ) {
-			descriptor = Object.getOwnPropertyDescriptor( object, propertyName );
-			$setter = descriptor.set;
+        "setter": function setter( target, propertyName, onSetter ) {
+            var descriptor, setter;	// variable setter refers to setter( target, propertyName, onSetter ) itself
 
-			if ( Object.prototype.hasOwnProperty.call( descriptor, "set" ) &&
-				typeof $setter === "function" ) {
-				descriptor.set = Hooks.fn( $setter, onSet );
-			} else {
-				throw new Error( "ERR_NOT_A_SETTER" );
-			}
+            if ( Object.prototype.hasOwnProperty.call( target, propertyName ) ) {
+                descriptor = Object.getOwnPropertyDescriptor( target, propertyName );
+                setter = descriptor.set;
 
-			Object.defineProperty( object, propertyName, descriptor );
-		} else {
-			throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
-		}
-	},
+                if ( Object.prototype.hasOwnProperty.call( descriptor, "set" ) &&
+                    "function" === typeof setter ) {
+                    descriptor.set = Hooks.apply( setter, function( setter, thisArg, argv ) {
+                        return onSetter.call( this, target, propertyName, setter, thisArg, argv );
+                    } );
+                } else {
+                    throw new Error( "ERR_NOT_A_SETTER" );
+                }
 
-	"method": function method( object, methodName, onInvoke ) {
-		"use strict";
+                Object.defineProperty( target, propertyName, descriptor );
+            } else {
+                throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
+            }
+        },
 
-		var $method;	// variable method refers to function method( object, methodName, onInvoke ) itself
+        "method": function method( target, methodName, onApply ) {
+            var method;
 
-		if ( Object.prototype.hasOwnProperty.call( object, methodName ) ) {
-			$method = object[ methodName ];
+            if ( Object.prototype.hasOwnProperty.call( target, methodName ) ) {
+                method = target[ methodName ];
 
-			if ( typeof $method === "function" ) {
-				object[ methodName ] = Hooks.fn( $method, onInvoke );
-			} else {
-				throw new Error( "ERR_NOT_A_METHOD" );
-			}
-		} else {
-			throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
-		}
-	}
-};
+                if ( "function" === typeof method ) {
+                    target[ methodName ] = Hooks.apply( method, function( method, thisArg, argv ) {
+                        return onApply.call( this, target, methodName, method, thisArg, argv );
+                    } );
+                } else {
+                    throw new Error( "ERR_NOT_A_METHOD" );
+                }
+            } else {
+                throw new Error( "ERR_PROPERTY_NOT_DEFINED" );
+            }
+        }
+    };
+
+    return Hooks;
+} )();
