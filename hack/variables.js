db6097081e829a65d6e12fd99220df909f975096
Xu Yiming <xuyiming.open@outlook.com>
2019-01-12 22:54:45 +0800

    feat(hack/variables.js): Get global variables

---
+++ hack/variables.js
@@ -0,0 +1,23 @@
+/**
+ * returns variables declared globally
+ */
+export const variables = () => {
+	const frame = document.createElement( "iframe" ),
+		vars = {};
+
+	frame.style.display = "none";
+	document.body.appendChild( frame );
+
+	const emptyWindow = frame.contentWindow;
+
+	Reflect.ownKeys( window )
+		.filter( name => !emptyWindow.hasOwnProperty( name ) )
+		.reduce( ( map, name ) => {
+			map[ name ] = window[ name ];
+			return map;
+		}, vars );
+
+	document.body.removeChild( frame );
+
+	return vars;
+};
