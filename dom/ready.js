d8211c9eaa5de7904d738892f6a660f6fe3e8141
Xu Yiming <xuyiming.open@outlook.com>
2019-01-12 22:37:59 +0800

    feat(dom/ready.js): Promisify loading process of `EventTarget` with "load" and "error" event

---
+++ dom/ready.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @template {TypedEventTarget<"load" | "error">} T
+ * @param {T} target
+ * @return {Promise<T>}
+ */
+export const ready = target =>
+	new Promise( ( resolve, reject ) => {
+		/**
+		 * @param {ErrorEvent} ev
+		 */
+		const rejectHandler = ev => reject( ev.error );
+
+		target.addEventListener( "load", () => resolve( target ) );
+		target.addEventListener( "error", rejectHandler );
+	} );
+
+
+/**
+ * @template E
+ * @typedef TypedEventTarget
+ * @property {(event: Event) => boolean} dispatchEvent
+ * @property {(type: E, listener: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions) => void} addEventListener
+ * @property {(type: E, listener: EventListenerOrEventListenerObject | null, options?: boolean | AddEventListenerOptions) => void} removeEventListener
+ */
