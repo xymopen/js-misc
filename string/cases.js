e01c0f211de24538d272e5bd73b07e45a9f0501d
Xu Yiming <xuyiming.open@outlook.com>
2019-01-12 17:32:50 +0800

    feat(string/cases.js): Convert between cases

---
+++ string/cases.js
@@ -0,0 +1,11 @@
+/**
+ * Convert str from kebab-case to camelCase
+ * @param {string} str
+ */
+const camelCase = str => str.replace( /-[a-z]/g, $0 => $0.charAt( 1 ).toUpperCase() );
+
+/**
+ * Convert str from camelCase to kebab-case
+ * @param {string} str
+ */
+const kebabCase = str => str.replace( /[A-Z]/g, $0 => `-${ $0.toLowerCase() }` );
