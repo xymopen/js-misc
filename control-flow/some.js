5a05c0270791a4cf8b44ec5baf9c5e0bb2531e40
Xu Yiming <xuyiming.open@outlook.com>
2019-01-15 01:35:26 +0800

    feat(control-flow/some.js): Resolves when some promises resolved

---
+++ control-flow/some.js
@@ -0,0 +1,34 @@
+/**
+ * resolves when any promise resolves,
+ * rejects when all promises rejected
+ * @template T
+ * @param {(T | PromiseLike<T>)[]} promises
+ */
+export const some = async promises => {
+	/**
+	 * @type {any[]}
+	 */
+	const reasons = [];
+	/**
+	 * @type {T[] & {errors?: any[]}}
+	 */
+	let values = [];
+
+	for ( let promise of promises ) {
+		try {
+			values.push( await promise );
+		} catch ( error ) {
+			reasons.push( error );
+		}
+	}
+
+	if ( values.length > 0 ) {
+		if ( reasons.length > 0 ) {
+			values.errors = reasons;
+		}
+
+		return values;
+	} else {
+		throw reasons;
+	}
+};
