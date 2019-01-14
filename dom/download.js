892f7f32ebe095866903e41a7c9b2620a42bd5b0
Xu Yiming <xuyiming.open@outlook.com>
2019-01-15 01:12:35 +0800

    feat(dom/download.js): Download files from url with given filename

---
+++ dom/download.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {string} url
+ * @param {string} filename - only effective when url is from same origin to document
+ */
+export const download = ( url, filename = new URL( url ).pathname.split( "/" ).pop() ) => {
+	const a = document.createElement( "a" );
+
+	a.download = filename;
+	a.href = url;
+	a.rel = "noopener"
+	a.target = "_blank";
+
+	a.click();
+}
