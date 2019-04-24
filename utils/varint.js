c7db8c7b65fa46e9e36ee1d8a57c02c203fc2c66
Xu Yiming <xuyiming.open@outlook.com>
2019-04-24 16:46:43 +0800

    feat(utils/varint.js): Encode and decode Protobuf varint

---
+++ utils/varint.js
@@ -0,0 +1,33 @@
+/**
+ * @see [Protobuf Encoding](https://developers.google.com/protocol-buffers/docs/encoding
+ * "Encoding | Protocol Buffers | Google Developers")
+ */
+
+"break jsdoc";
+
+
+/** @param {number} n */
+export const encode = function* ( n ) {
+	while ( n > 0b01111111 ) {
+		yield n & 0b01111111 | 0 << 8;
+		n >>>= 7;
+	}
+
+	yield n | 1 << 7;
+};
+
+export const decode = function* () {
+	/** @type {number} n */
+	let n = 0, i = 0;
+
+	while ( true ) {
+		const m = yield;
+
+		n |= ( m & 0b01111111 ) << ( i * 7 );
+		i += 1;
+
+		if ( m >>> 7 === 1 ) {
+			return n;
+		}
+	}
+};
