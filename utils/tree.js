8d9a6a8a1224213166e00e5c11f42d6878a002fd
Xu Yiming <xuyiming.open@outlook.com>
2019-09-22 11:54:38 +0800

    feat(utils/tree.js): List contents in a tree-like format

---
+++ utils/tree.js
@@ -0,0 +1,105 @@
+/**
+ * @template T
+ * @param {string} title
+ * @param {T} node
+ * @param {(context: T) => Iterator<[string] | [string, T]>} deepen
+ * @param {string} ps1 - prompt string for first line of normal entry
+ * @param {string} ps2 - prompt string for normal line of normal entry
+ * @param {string} ps3 - prompt string for first line of last entry
+ * @param {string} ps4 - prompt string for normal line of last entry
+ * @param {string} is1 - indent string for normal entry
+ * @param {string} is2 - indent string for last entry
+ */
+function* treeNode(
+	title, node, deepen,
+	ps1, ps2,
+	ps3, ps4,
+	is1, is2
+) {
+	yield `${ is1 }${ title }`;
+
+	if ( node !== undefined ) {
+		const children = deepen( node );
+		const result = children.next();
+
+		if ( !result.done ) {
+			/** @type {[string, T]} */
+			let lastValue = result.value;
+
+			while ( true ) {
+				const result = children.next();
+				const [ title, child ] = lastValue;
+
+				if ( result.done ) {
+					yield* treeNode(
+						title,
+						child,
+						deepen,
+						ps1, ps2,
+						ps3, ps4,
+						`${ is2 }${ ps3 }`,
+						`${ is2 }${ ps4 }`
+					);
+
+					break;
+				} else {
+					yield* treeNode(
+						title,
+						child,
+						deepen,
+						ps1, ps2,
+						ps3, ps4,
+						`${ is2 }${ ps1 }`,
+						`${ is2 }${ ps2 }`
+					);
+
+					lastValue = result.value;
+				}
+			}
+		}
+	}
+};
+
+/**
+ * List contents in a tree-like format
+ * 
+ * @template T
+ * @param {string} title
+ * @param {T} node
+ * @param {(context: T) => Iterator<[string] | [string, T]>} deepen
+ * @param {string} ps1 - prompt string for first line of normal entry
+ * @param {string} ps2 - prompt string for normal line of normal entry
+ * @param {string} ps3 - prompt string for first line of last entry
+ * @param {string} ps4 - prompt string for normal line of last entry
+ * @example ```
+ * // Shell TREE(1)
+ * for ( const line of tree( process.cwd(), process.cwd(), function* ( dirname ) {
+ * 	for ( const filename of fs.readdirSync( dirname ) ) {
+ * 		const pathname = path.join( dirname, filename );
+ *
+ * 		if ( fs.statSync( pathname ).isDirectory() ) {
+ * 			yield [ filename, pathname ];
+ * 		} else {
+ * 			yield [ filename ];
+ * 		}
+ * 	}
+ * } ) ) {
+ * 	console.log( line );
+ * }
+ * ```
+ */
+export function* tree(
+	title, node, deepen,
+	ps1 = "+-- ", ps2 = "|   ",
+	ps3 = "`-- ", ps4 = "    "
+) {
+	yield* treeNode(
+		title,
+		node,
+		deepen,
+		ps1, ps2,
+		ps3, ps4,
+		"",
+		""
+	);
+};
