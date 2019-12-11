87b5cff14770fc9a12d8af587f0e5b2e48e8d4f1
Xu Yiming <xuyiming.open@outlook.com>
2019-12-11 23:07:03 +0800

    fix(control-flow/promisify-iterator.js): Duplicate iterator and improve typing

--- control-flow/promisify-iterator.js
+++ control-flow/promisify-iterator.js
@@ -1,19 +1,16 @@
 import { makePromise } from "../utils/make-promise.js";
 
 /**
- * Obtain the return value of an iterator
- * or capture its error
- *
  * @template T
- * @param {Iterator<T>} it
- * @returns {Promise<T>}
+ * @template {NextResult<T>} F
+ * @param {F} next
+ * @param {PromiseResolver<T>} resolve
+ * @param {PromiseRejecter} reject
+ * @returns {SameFunction<F>}
  */
-export const promisifyIterator = it => {
-	const next = it.next,
-		{ promise, resolve, reject } = makePromise();
-
-	it.next = function ( ...args ) {
-		/** @type {IteratorResult<T>} */
+const makeNext = ( next, resolve, reject ) =>
+	function ( ...args ) {
+		/** @type {ReturnType<F>} */
 		let result;
 
 		try {
@@ -31,5 +28,68 @@ export const promisifyIterator = it => {
 		}
 	};
 
-	return promise;
+/**
+ * Obtain the return value of an iterator
+ * or capture its error
+ *
+ * @template {Iterator<any>} I
+ * @param {I} it
+ * @returns {Promisified<I>}
+ */
+export const promisifyIterator = it => {
+	const { promise, resolve, reject } = makePromise();
+	/** @type {Pick<I, keyof Iterator<any>>} */
+	const iterator = {};
+
+	iterator.next = makeNext( it.next.bind( it ), resolve, reject );
+
+	if ( typeof it.return === "function" ) {
+		iterator.return = makeNext( it.return.bind( it ), resolve, reject );
+	}
+
+	if ( typeof it.throw === "function" ) {
+		iterator.throw =makeNext( it.throw.bind( it ), resolve, reject );
+	}
+
+	return {
+		promise,
+		iterator
+	};
 };
+
+/**
+ * @template T
+ * @typedef {import("../utils/make-promise.js").PromiseResolver<T>} PromiseResolver
+ */
+
+/**
+ * @typedef {import("../utils/make-promise.js").PromiseRejecter} PromiseRejecter
+ */
+
+/**
+ * @template T
+// @ts-ignore
+ * @typedef {T extends Iterator<any, infer P> ? P : never} IteratorReturnType
+ */
+
+/**
+ * @template {Function} F
+ * @callback SameFunction
+ * @param {...Parameters<F>} args
+ * @returns {ReturnType<F>}
+ */
+
+/**
+ *
+ * @template T
+ * @callback NextResult
+ * @param {...any[]} args
+ * @returns {IteratorResult<any, T>}
+ */
+
+/**
+ * @template {Iterator<any>} I
+ * @typedef Promisified
+ * @property {Promise<IteratorReturnType<I>>} promise
+ * @property {Pick<I, keyof Iterator<any>>} iterator
+ */
