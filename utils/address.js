ff0894735daae75b0a1eccb4965d78af37767f10
Xu Yiming <xuyiming.open@outlook.com>
2020-01-02 15:05:07 +0800

    refactor(utils/address.js): Improve JSDoc and typing

--- utils/address.js
+++ utils/address.js
@@ -1,15 +1,33 @@
-// Row major order, 0 indexed, C
-// ( ( y ) * width + x ) * channels + c
-const addr = ( indexes, shape ) =>
-	indexes.reduce(
-		( offset, index, i ) =>
-			offset * shape[ i ] + index
-	);
+/**
+ * Calculate the position of an element in a flatten, row
+ * major ordered(C style), 0-based, multi-dimensional array
+ *
+ * `0` is used if an index is missing in the `indices`
+ *
+ * For example, to address a value in an `ImageData`,
+ * evaluate `( y * width + x ) * 3 + c`.
+ *
+ * @param {number[]} indices - Row major ordered array indices
+ * @param {number[]} shape - Row major ordered array shape
+ * @returns {number}
+ */
+export const addr = ( indices, shape ) =>
+	shape.reduce(( offset, axis, i ) =>
+			offset * axis + ( indices[ i ] ?? 0 ));
 
-// Column major order, 0 indexed, Fortran
-// y + height * ( x + width * ( c ) )
-const addrCMO = ( indexes, shape ) =>
-	indexes.reduceRight(
-		( offset, index, i ) =>
-			index + shape[ i ] * offset
-	);
+/**
+ * Calculate the position of an element in a flatten, column
+ * major ordered(Fortran style), 0-based, multi-dimensional array
+ *
+ * `0` is used if an index is missing in the `indices`
+ *
+ * For example, to address a value in an `ImageData`,
+ * evaluate `y + height * ( x + width * c )`.
+ *
+ * @param {number[]} indices - Column major ordered array indices
+ * @param {number[]} shape - Column major ordered array shape
+ * @returns {number}
+ */
+export const addrTransposed = ( indices, shape ) =>
+	shape.reduceRight(( offset, axis, i ) =>
+			( indices[ i ] ?? 0 ) + offset * axis);
