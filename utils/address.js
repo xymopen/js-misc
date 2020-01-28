2ce3fa2f2c7834d98ed6e3e0e386afd6f72f7326
Xu Yiming <xuyiming.open@outlook.com>
2020-01-29 01:34:21 +0800

    fix(utils/address.js): Correct calculation

--- utils/address.js
+++ utils/address.js
@@ -1,5 +1,13 @@
 /**
- * Calculate the position of an element in a flatten, row
+ * @see [Row- and column-major order](
+ * https://en.wikipedia.org/wiki/Row-_and_column-major_order
+ * "Row- and column-major order - Wikipedia")
+ */
+
+"break jsdoc";
+
+/**
+ * Calculate the position of an element in a flatten, row-
  * major ordered(C style), 0-based, multi-dimensional array
  *
  * `0` is used if an index is missing in the `indices`
@@ -7,16 +15,17 @@
  * For example, to address a value in an `ImageData`,
  * evaluate `( y * width + x ) * 4 + c`.
  *
- * @param {number[]} indices - Row major ordered array indices
- * @param {number[]} shape - Row major ordered array shape
+ * @param {number[]} indices
+ * @param {number[]} shape
  * @returns {number}
+ * @example addr( [ y, x, c ], [ height, width, 4 ] );
  */
 export const addr = ( indices, shape ) =>
-	shape.reduce(( offset, axis, i ) =>
-			offset * axis + ( indices[ i ] ?? 0 ));
+	shape.slice( 1 ).reduce( ( offset, axis, i ) =>
+		offset * axis + ( indices[ i + 1 ] ?? 0 ), indices[ 0 ] ?? 0 );
 
 /**
- * Calculate the position of an element in a flatten, column
+ * Calculate the position of an element in a flatten, column-
  * major ordered(Fortran style), 0-based, multi-dimensional array
  *
  * `0` is used if an index is missing in the `indices`
@@ -24,10 +33,11 @@ export const addr = ( indices, shape ) =>
  * For example, to address a value in an `ImageData`,
  * evaluate `y + height * ( x + width * c )`.
  *
- * @param {number[]} indices - Column major ordered array indices
- * @param {number[]} shape - Column major ordered array shape
+ * @param {number[]} indices
+ * @param {number[]} shape
  * @returns {number}
+ * @example addrTransposed( [ y, x, c ], [ height, width, 4 ] );
  */
 export const addrTransposed = ( indices, shape ) =>
-	shape.reduceRight(( offset, axis, i ) =>
-			( indices[ i ] ?? 0 ) + offset * axis);
+	shape.slice( 0, -1 ).reduceRight( ( offset, axis, i ) =>
+		( indices[ i ] ?? 0 ) + axis * offset, indices[ indices.length - 1 ] ?? 0 );
